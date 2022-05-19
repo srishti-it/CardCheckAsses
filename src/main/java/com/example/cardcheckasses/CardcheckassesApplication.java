c0a8838c438b509f9f29dc6352e6c7e707c9e69c
srishti <srishtikashyap2013@gmail.com>
2022-05-19 14:16:25 +0530

    first commit

---
+++ src/main/java/com/example/cardcheckasses/CardcheckassesApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardcheckasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardcheckassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardcheckassesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cardcheckasses/CardcheckassesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardcheckasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardcheckassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
